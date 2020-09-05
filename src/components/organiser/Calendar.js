fbd62e49c37f79ace4613bd88ff4195d172bf774
NektoUa <illyabarkov@gmail.com>
2020-09-05 12:09:36 +0300

    add calendar

---
+++ src/components/organiser/Calendar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Calendar from 'rc-calendar';
+import DatePicker from 'rc-calendar/lib/Picker';
+import 'rc-calendar/assets/index.css';
+import moment from 'moment';
+
+class CalendarPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: moment()
+        }
+    }
+
+    render() {
+        const calendar = (<Calendar />);
+        return (
+            <div>
+                {/* Нужно ли пока вопрос? */}
+                {/* <DatePicker
+                    animation="slide-up"
+                    value={this.state.time}
+                    disabled={false}
+                    calendar={calendar}
+                    onChange={newTime => this.setState({ time: newTime })}
+                >{
+                        ({ value }) => {
+                            return (
+                                <input value={value ? value.format('YYYY-MM-DD HH:mm:ss') : ''} />
+                            )
+                        }
+                    }</DatePicker> */}
+                <Calendar />
+            </div>
+        )
+    }
+}
+
+export default CalendarPage;
--- src/components/organiser/Organiser.js
+++ src/components/organiser/Organiser.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Calendar from './Calendar';
 // import main from './Footer.module.css';
 
 export default class Organiser extends Component {
     render() {
         return (
             <div id="organiser">
-                ff
+                <Calendar />
 
             </div>
         );
